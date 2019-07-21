6502b0d98fc1529057ffccdfe019a273f35f39af
mariomui <yi.kan.mui@gmail.com>
2019-07-21 04:33:16 -0700

    Create get all vehicles information and get vehicle location
    2 get routes
    vehicle Location will get the first vehicle's location and serves
    as a proof of concept. vehicles route gets vehicleinfo, location, odometer
    of all user's vehicles.

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const client = new smartcar.AuthClient({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   redirectUri: process.env.REDIRECT_URI,
-  scope: ['required:read_vehicle_info'],
+  scope: ['required:read_vehicle_info','required:read_location','read_odometer', 'control_security', 'control_security:unlock', 'control_security:lock'],
   testMode: true,
 });
 
@@ -73,6 +73,72 @@ app.get('/vehicle', function (req, res) {
 
       res.json(info);
     });
+});
+//bmws are bad. teslas good.
+app.get('/vehiclelocation', function (req, res) {
+  return smartcar.getVehicleIds(access.accessToken)
+    .then(function (data) {
+      // the list of vehicle ids
+      return data.vehicles;
+    })
+    .then(function (vehicleIds) {
+      // instantiate the first vehicle in the vehicle id list
+      const vehicle = new smartcar.Vehicle(vehicleIds[0], access.accessToken);
+      // vehicle.unlock();
+      return vehicle.location();
+    })
+    .then(function (location) {
+      console.log(location);
+      res.json(location);
+    });
+});
+/**
+ * summary: after /login, -> this route will return storage with all the cars that you control.
+ */
+app.get('/vehicles', (req, res) => {
+  const { accessToken } = access;
+  let promisedCarData = [];
+  return smartcar.getVehicleIds(accessToken)
+    .then((data) => {
+      return data.vehicles;
+    })
+    .then((vehicleIds) => {
+      const promisedCarsData = [];
+      vehicleIds.forEach((vehicleId) => {
+        const vehicle = new smartcar.Vehicle(vehicleId, accessToken)
+        promisedCarData = [vehicle.info(), vehicle.location(), vehicle.odometer()]
+        promisedCarsData.push(Promise.all(promisedCarData));
+      })
+      return promisedCarsData;
+    })
+    .then((promisedCarsData) => {
+      const metaLabel = ['info', 'location', 'odometer']
+      const storage = [];
+      promisedCarsData.forEach((promisedCarData) => {
+        promisedCarData.then((carData) => {
+          const carFullData = {};
+          carData.reduce((accum, carDatum, key) => {
+            accum[ metaLabel[ key ] ] = carDatum;
+            return accum;
+          }, carFullData)
+          return carFullData;
+        })
+        .then((carFullData) => {
+          storage.push(carFullData);
+          if (storage.length === promisedCarsData.length) {
+            console.log(storage); //storage will have info of every car.
+            res.json(storage).redirect();
+/* 0: Object {info: Object, location: Object, odometer: Object}
+info: Object {id: "3d9558a6-b0bb-4270-b5c6-19edcde2787c", make: "TESLA", model: "Model 3", year: 2017}
+location: Object {data: {latitude: 37.55, longtitude: 37.55}, age: Sun Jul 21 2019 04:04:32 GMT-0700 (Pacific Dayligh…} // age might be an Date obj...
+odometer: Object {data: {distance: 1222067}, age: Sun Jul 21 2019 04:04:32 GMT-0700 (Pacific Dayligh…, unitSystem: "metric"}
+....
+*/
+          }
+        })
+      })
+      return storage;
+    })
 });
 
 app.listen(PORT, () => console.log(`listening on localhost:${PORT}`));
