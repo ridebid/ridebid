97e121ee30df161399aa84ec23c542f8cd85b2e7
mariomui <yi.kan.mui@gmail.com>
2019-07-21 11:13:21 -0700

    Added more css to car page

--- src/Container/CarCard.jsx
+++ src/Container/CarCard.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
-  const CarCard = (props) => {
-    const { info, location, odometer } = props.car;
-    const styleRow = {    
-      width: '50vh'
-    }
-    const styleCard = {
-      borderRadius: '13px',
-      width: '351px',
-      height: '268px',
-    }
+const CarCard = (props) => {
+  const { info, location, odometer } = props.car;
+  const styleRow = {    
+    width: '50vh'
+  }
+  const styleCarded = {
+    borderRadius: '13px',
+    width: '351px',
+    height: '268px'
+  }
   return (
-    <div>
-
-
     <div style={styleRow}>
-      <Card style={styleCard}>
+      <Card style={styleCarded}>
         <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-        <CardBody style={{align: 'center'}}>
+        <CardBody>
           <CardTitle>      {info.make}</CardTitle>
           <CardSubtitle>{info.year}</CardSubtitle>
           <CardText></CardText>
           <Button>Button</Button>
         </CardBody>
       </Card>
-    </div>
     </div>
   );
 };
